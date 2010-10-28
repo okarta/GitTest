799e57f4c650c759413c8e12e3aa9c87c99c474d
Constantin A <okarta@gmail.com>
2010-10-28 15:14:30 +0400

    Signed-off-by: Constantin A <okarta@gmail.com>

--- index.php
+++ index.php
@@ -1,39 +1,59 @@
-<html>
-<body>
 <?php
 
-    $hivext->local->SetHeader("Content-Type", "text/html");
+    /*
+        Name: GetUsersJoinCountryAndCity
 
-    session_start();
-    
-    $auth = $hivext->users->authentication;
+        Data model:
 
-    // Check authentication.
-    $resp = $auth->CheckSign($appid, $_SESSION["user_session"]);
-    if($resp->result == 0) { // Already authenticated.
-        $_SESSION["user_session"] = $resp->session;
-        echo "Your email " . $resp->email . " and UserID = " . $resp->uid; // Show user email & id.
-        echo "<br /><a href='Signout'>Logout</a>";
-
-    } else { // Not authenticated.
-
-        echo '<form method="post" action="Signin">';
-        echo 'Email <input name="login" type="text" value="guest@guest.com">&nbsp;&nbsp;';
-        echo 'Password <input name="password" type="text" value="guest">&nbsp;&nbsp;';
-        echo '<input type="submit" value="Login">';
-        echo '</form>';
-
-        if($login != "" && $password != "") {
-            $resp = $auth->Signin($appid, $login, $password);
-            if($resp->result == 0) { // Authentication success.
-                $_SESSION["user_session"] = $resp->session;
-                return $hivext->local->Redirect("Signin");
-            } else {
-                echo $resp->result . " : " . $resp->error; // Show error & result.
+            JoinSample_Country : {
+                value : string
             }
-        }
+
+            JoinSample_City : {
+                value : string
+            }
+
+            JoinSample_Address : {
+                country : JoinSample_Country,
+                city : JoinSample_City
+            }
+
+            JoinSample_User : {
+                name : string,
+                address : JoinSample_Address
+            }
+
+        Type description:
+
+            JoinSample_User - users objects, store address reference
+            JoinSample_Address - addresses objects store country and city reference
+            JoinSample_Country - country list
+            JoinSample_City - city list
+    */
+
+    $db = $hivext->data->base; // Request data base service.
+
+    $hivext->local->SetHeader("Content-Type", "text/plain");
+    
+    // Request objects, join address type.
+    $users = $db->GetObjects(
+        $appid, // current application
+        $signature, // node signature
+        "JoinSample_User", // type name
+        null, // from, null - skip param
+        null, // count, null - skip param
+        "address, address.country, address.city" // join 
+                                                 // address : JoinSample_Address
+                                                 // address.country : JoinSample_Country
+                                                 // address.city : JoinSample_City
+    );
+
+    if($users->result == 0) { // Success.
+        for($i = 0; $i < count($users->objects); $i++) {
+            echo "name = " . $users->objects[$i]->name
+                ."\ncountry = " . $users->objects[$i]->address->country->value
+                ."\ncity = " . $users->objects[$i]->address->city->value . "\n\n";
+         }
     }
 
 ?>
-</body>
-</html>
